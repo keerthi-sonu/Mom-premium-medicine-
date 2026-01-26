580b3b6747b3d28ab54bd4d29f3b886707a1483b
manohar1030 <manoharvaraprasad2@gmail.com>
2026-01-26 16:42:45 +0530

    added medicines page like cancer & sugar

---
+++ js/cards.js
@@ -0,0 +1,10 @@
+//cancer card
+document.getElementById("cancerCard").addEventListener("click",function(){
+  window.location.href="cancer.html";
+});
+
+//sugar card
+document.getElementById("sugarCard").addEventListener("click",function(){
+  window.location.href="sugar.html";
+})
+
