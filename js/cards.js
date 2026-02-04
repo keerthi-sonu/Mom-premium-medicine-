13f0620f44d9422650915982315b94cd78bc73d4
manohar1030 <manoharvaraprasad2@gmail.com>
2026-02-04 18:13:10 +0530

    minor issues fixed

--- js/cards.js
+++ js/cards.js
@@ -1,9 +1,9 @@
-//cancer card
+//cancer card opening
 document.getElementById("cancerCard").addEventListener("click",function(){
   window.location.href="cancer.html";
 });
 
-//sugar card
+//sugar card opening
 document.getElementById("sugarCard").addEventListener("click",function(){
   window.location.href="sugar.html";
 })
